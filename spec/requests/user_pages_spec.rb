848ac7050626133023a6e4a726bb94e4d21739a4
irimon <ilanrimon@yahoo.com>
2013-04-26 14:59:18 +0300

    Finish sign in

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -63,7 +63,10 @@ describe "User pages" do
 
         it { should have_selector('title', text: user.name) }
         it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+		it { should have_link('Sign out') }
+
       end
+	  
 	  
     end
   end
