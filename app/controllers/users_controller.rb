2c5421c8210fb1882dab5a84f3f394bc5836985a
irimon <ilanrimon@yahoo.com>
2013-05-04 20:54:26 +0300

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,9 @@ class UsersController < ApplicationController
   before_filter :correct_user,   only: [:edit, :update]
   before_filter :admin_user,     only: :destroy
   
-  def show
+   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def new
@@ -49,13 +50,7 @@ class UsersController < ApplicationController
   
   private
 
-    def signed_in_user
-		unless signed_in?
-			store_location
-			redirect_to signin_url, notice: "Please sign in."
-		end
-	end
-	
+   	
 	def correct_user
       @user = User.find(params[:id])
       redirect_to(root_path) unless current_user?(@user)
